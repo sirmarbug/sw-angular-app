1f1c6919a887caba220d1bd544feaf3ed1ee28ac
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-02 19:10:03 +0100

    Fixed bug

--- src/app/shared/components/persons-timeline/persons-timeline.component.ts
+++ src/app/shared/components/persons-timeline/persons-timeline.component.ts
@@ -23,13 +23,13 @@ export class PersonsTimelineComponent implements OnInit {
 
   private groupInit(): void {
     this.data = [
-      new GroupPerson('Unknown', new Array<Person>()),
       new GroupPerson('0 - 20 BBY', new Array<Person>()),
       new GroupPerson('20 - 40 BBY', new Array<Person>()),
       new GroupPerson('40 - 60 BBY', new Array<Person>()),
       new GroupPerson('60 - 80 BBY', new Array<Person>()),
       new GroupPerson('80 - 100 BBY', new Array<Person>()),
-      new GroupPerson('100 BBY++', new Array<Person>())
+      new GroupPerson('100 BBY++', new Array<Person>()),
+      new GroupPerson('Unknown', new Array<Person>())
     ];
   }
 
@@ -61,34 +61,35 @@ export class PersonsTimelineComponent implements OnInit {
   private groupPeople(): void {
     this.people.forEach((people: Person) => {
       if (isNaN(people.birthYear)) {
-        this.data[0].people.push(people);
-        return;
-      }
-      if (people.birthYear < 20) {
-        this.data[1].people.push(people);
-        return;
-      }
-      if (people.birthYear >= 20 && people.birthYear < 40) {
-        this.data[2].people.push(people);
-        return;
-      }
-      if (people.birthYear >= 40 && people.birthYear < 60) {
-        this.data[3].people.push(people);
-        return;
-      }
-      if (people.birthYear >= 60 && people.birthYear < 80) {
-        this.data[4].people.push(people);
-        return;
-      }
-      if (people.birthYear >= 80 && people.birthYear < 100) {
-        this.data[5].people.push(people);
-        return;
-      }
-      if (people.birthYear >= 100) {
         this.data[6].people.push(people);
         return;
       }
+      if (people.birthYear < 20) {
+        this.data[0].people.push(people);
+        return;
+      }
+      if (people.birthYear >= 20 && people.birthYear < 40) {
+        this.data[1].people.push(people);
+        return;
+      }
+      if (people.birthYear >= 40 && people.birthYear < 60) {
+        this.data[2].people.push(people);
+        return;
+      }
+      if (people.birthYear >= 60 && people.birthYear < 80) {
+        this.data[3].people.push(people);
+        return;
+      }
+      if (people.birthYear >= 80 && people.birthYear < 100) {
+        this.data[4].people.push(people);
+        return;
+      }
+      if (people.birthYear >= 100) {
+        this.data[5].people.push(people);
+        return;
+      }
     });
+    console.log('group', this.data);
   }
 
 }
